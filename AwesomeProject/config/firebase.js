65985ecd9d62f8a976dc2f5253af4c616df7d3bc
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2024-04-05 19:06:44 -0400

    real time chat setup

---
+++ AwesomeProject/config/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import Constants from "expo-constants"; 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  authDomain: Constants.expoConfig.extra.authDomain,
+  projectId: Constants.expoConfig.extra.projectId,
+  storageBucket: Constants.expoConfig.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+  appId: Constants.expoConfig.extra.appId,
+  databaseURL: Constants.expoConfig.extra.databaseURL
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
